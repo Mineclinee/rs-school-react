eed422f73546cadbe867785606c620698b6ac544
Mineclinee <vasily-minecline@yandex.ru>
2024-07-04 22:17:08 +0300

    refactor: remove unnecessary styles, refactor error in app.tsx, refactor project structure

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     if (this.state.hasError) {
       return (
         <h1>
-          Something went wrong.{' '}
+          Something went wrong.
           <button onClick={() => this.setState({ hasError: false })}>
             Try again
           </button>
@@ -69,7 +69,7 @@ class App extends Component<object, AppState> {
     const token =
       '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';
     fetch(
-      `https://api.github.com/search/repositories?q=${query}&per_page=25&page=1`,
+      `https://api.github.com/search/repositories?q=${query}&per_page=50&page=1`,
       {
         headers: {
           authorization: `token ${token}`,
@@ -107,7 +107,13 @@ class App extends Component<object, AppState> {
     return (
       <ErrorBoundary>
         <div className="app">
-          <h1 className="app__title">Search for repositories in GitHub 👨‍💻</h1>
+          <div className="app__info">
+            <h1 className="app__title">Search for repositories in GitHub 👨‍💻</h1>
+            <p className="app__descr">
+              Enter a name and get the first 50 results in the format: "Account
+              Name/Repository".
+            </p>
+          </div>
           <div className="app__search">
             <input
               className="form-field__input input-reset"
@@ -129,16 +135,19 @@ class App extends Component<object, AppState> {
                 <div className="loader"></div>
               </div>
             )}
-            {error && <p>Error loading results.</p>}
-            <ul className="app__list list-reset">
-              {results && results.length > 0
-                ? results.map((result) => (
-                    <li className="app__item" key={result.id}>
-                      {result.full_name}
-                    </li>
-                  ))
-                : !loading && <p>No results found</p>}
-            </ul>
+            {error ? (
+              <p className="app__error">Error loading results. 😿</p>
+            ) : (
+              <ul className="app__list list-reset">
+                {results && results.length > 0
+                  ? results.map((result) => (
+                      <li className="app__item" key={result.id}>
+                        {result.full_name}
+                      </li>
+                    ))
+                  : !loading && <li>No results found 🥺</li>}
+              </ul>
+            )}
           </div>
         </div>
       </ErrorBoundary>
